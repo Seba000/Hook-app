06daf069d30e5d4674ea7035a50690a93a287ae2
Sebastian Letelier <sebalete10@gmail.com>
2022-11-19 14:53:32 -0300

    useRef

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 //--
-import { MultipleCustomHooks } from "./03-ejemplos/MultipleCustomHooks";
+import { FocusScreen } from "./04-useRef/FocusScreen";
+//import { MultipleCustomHooks } from "./03-ejemplos/MultipleCustomHooks";
 //import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
 //import { SimpleForm } from "./02-useEffect/SimpleForm";
 //import { HooksApp } from "./HooksApp";
@@ -12,6 +13,6 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MultipleCustomHooks />
+    <FocusScreen />
   </React.StrictMode>
 );
