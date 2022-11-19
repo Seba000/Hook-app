bc758acae3e247006fe9b5f8bc57c6fe45e46ee6
Sebastian Letelier <sebalete10@gmail.com>
2022-11-19 13:42:42 -0300

    citas de breaking bad, con un custom hook de fetch y un custom hook de counter,lo que muestra el author y la quote y deja mostrar previous and next quote con el boton.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 //--
-import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
+import { MultipleCustomHooks } from "./03-ejemplos/MultipleCustomHooks";
+//import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
 //import { SimpleForm } from "./02-useEffect/SimpleForm";
 //import { HooksApp } from "./HooksApp";
 //import { CounterApp } from "./01-useState/CounterApp";
@@ -11,6 +12,6 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FormWithCustomHook />
+    <MultipleCustomHooks />
   </React.StrictMode>
 );
